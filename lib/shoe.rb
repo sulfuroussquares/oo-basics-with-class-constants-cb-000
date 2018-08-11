e96142d01bd78fadb0cb159d021562855b06cc0a
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-08-11 15:47:41 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,13 +6,7 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    if (BRANDS.include?(brand))
-      BRANDS << brand
-    end
-    BRANDS
-    #!BRANDS.include?(brand) ? BRANDS << brand : BRANDS
-    #BRANDS << brand
-
+    !BRANDS.include?(brand) ? BRANDS << brand : BRANDS
   end
 
 
