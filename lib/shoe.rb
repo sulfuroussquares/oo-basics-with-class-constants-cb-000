27afd43f742bef03499f6ddb1b087be3b02b087a
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-08-11 15:26:46 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,10 +1,11 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition, :brand
-  attr_reader :brand
+  #attr_reader :brand
 
   def initialize(brand)
     @brand = brand
   end
+  
   BRANDS = []
 
   def brand=(brand)
